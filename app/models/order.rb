af1c0dc089b244aba7145d5843ca1a0ea4c77196
zsmhy <zsm@example.com>
2017-05-18 22:39:10 +0800

    changes order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,4 @@ class Order < ApplicationRecord
   vaildates :billing_address, presence: true
   vaildates :shipping_name, presence: true
   vaildates :shipping_address, presence: true
-  # validates :billing_name, presence: true
-  # validates :billing_address, presence: true
-  # validates :shipping_name, presence: true
-  # validates :shipping_address, presence: true
 end
